b79698adf95a0c9714a588d9b85c7f8211003810
nicoechaniz <nicoechaniz@altermundi.net>
2017-03-20 20:04:54 -0300

    refactor and add tests for new api endpoints

--- apitest.py
+++ apitest.py
@@ -2,49 +2,86 @@ from websocket import create_connection
 import json
 import hashlib
 
-USER = r"admin"
-PASSWORD = r"admin"
+class LiMeApi(object):
+    def __init__(self, url):
+        self.connection = create_connection(url)
+        self.call_id = 0
 
-def ws_send(call):
-    print("Sending", call)
+    def _send(self, method, params):
+        self.call_id += 1
+        call = {"jsonrpc": "2.0", "id":self.call_id, "method": method, "params": params}
+        print("Sending", call)
+        self.connection.send(json.dumps(call))
+        print("Sent")
+        print("Receiving...")
+        result =  self.connection.recv()
+        print("Received '%s'" % result)
+        return result
 
-    ws.send(call)
-    print("Sent")
-    print("Receiving...")
-    result =  ws.recv()
-    return result
+    def list_methods(self):
+        return self._send(method="list", params=["","*"])
+
+    def challenge(self):
+        return self._send(method="challenge", params=[])
+
+    def login(self, challenge_result):
+        shapass = hashlib.new("sha1")
+        shatok = hashlib.new("sha1")
+        r = json.loads(challenge_result)
+        token = r["result"]["token"]
+        shapass.update(str.encode(password))
+        shatok.update(str.encode(token))
+        shatok.update(str.encode(shapass.hexdigest()))
+        passtok = shatok.hexdigest()
+
+        # login
+        res = self._send(method="login", params=[user, passtok])
+        r = json.loads(res)
+        success = r["result"]["success"]
+        return success
+
+    def get_hostname(self):
+        return self._send(method="call", params=[session_token,"/lime/api", "get_hostname",{}])
+
+    def get_location(self):
+        return self._send(method="call", params=[session_token,"/lime/api", "get_location",{}])
+
+    def get_neighbors(self):
+        return self._send(method="call", params=[session_token,"/lime/api", "get_neighbors",{}])
+
+    def get_bmx6(self):
+        return self._send(method="call", params=[session_token,"/lime/api", "get_bmx6",{}])
+
+    def set_location(self, lat, lon):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "set_location", {"lat":lat,"lon":lon}])
+    def get_interfaces(self):
+        return self._send(method="call", params=[session_token,"/lime/api", "get_interfaces",{}])
+
+    def get_stations(self, iface):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_stations", {"iface": iface}])
+
+    def close(self):
+        self.connection.close()
 
 if __name__ == "__main__":
-    shapass = hashlib.new("sha1")
-    shatok = hashlib.new("sha1")
+    user = r"admin"
+    password = r"admin"
 
-    ws = create_connection("ws://10.5.1.200/websocket/")
+    ws = LiMeApi("ws://10.13.207.235/websocket/")
 
-    # list
-    call = '{"jsonrpc":"2.0","id":1,"method":"list","params":["","*"]}'
-    res = ws_send(call)
+    ws.list_methods()
+    challenge_res = ws.challenge()
+    session_token = ws.login(challenge_res)
 
-    # challenge
-    call = '{"jsonrpc":"2.0","id":2,"method":"challenge","params":[]}'
-    res = ws_send(call)
-    r = json.loads(res)
-    token = r["result"]["token"]
-    shapass.update(str.encode(PASSWORD))
-    shatok.update(str.encode(token))
-    shatok.update(str.encode(shapass.hexdigest()))
+    res = ws.get_hostname()
+    res = ws.get_location()
+    res = ws.get_neighbors()
+    res = ws.get_bmx6()
+    res = ws.set_location(-31,-64)
+    res = ws.get_interfaces()
+    res = ws.get_stations("wlan1-adhoc")
 
-    passtok = shatok.hexdigest()
-
-    # login
-    call = '{"jsonrpc":"2.0","id":3,"method":"login","params":["%s","%s"]}' % (USER, passtok)
-    res = ws_send(call)
-    r = json.loads(res)
-    success = r["result"]["success"]
-
-    # hostname
-    call = '{"jsonrpc":"2.0","id":5,"method":"call",'\
-           '"params":["%s","/lime/api", "get_hostname",{}]}' % success
-
-    res = ws_send(call)
-    print("Received '%s'" % res)
     ws.close()
+
