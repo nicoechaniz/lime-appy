2d103b14af7a7281c65a63efc557d3b28ff9d67f
nicoechaniz <nicoechaniz@altermundi.net>
2017-04-18 03:55:28 -0300

    added api tests for signal levels and metrics

--- apitest.py
+++ apitest.py
@@ -57,16 +57,33 @@ class LiMeApi(object):
                                                  "set_location", {"lat":lat,"lon":lon}])
     def get_interfaces(self):
         return self._send(method="call", params=[session_token,"/lime/api", "get_interfaces",{}])
-
-    def get_stations(self, iface):
+    def get_iface_stations(self, iface):
         return self._send(method="call", params=[session_token,"/lime/api",
-                                                 "get_stations", {"iface": iface}])
+                                                 "get_iface_stations", {"iface": iface}])
+    def get_stations(self):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_stations", {}])
     def get_station_signal(self, iface, station_mac):
         return self._send(method="call", params=[session_token,"/lime/api", "get_station_signal",
                                                  {"iface": iface, "station_mac": station_mac}])
     def get_assoclist(self, iface):
         return self._send(method="call", params=[session_token,"/lime/api",
                                                  "get_assoclist", {"iface": iface}])
+    def get_gateway(self):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_gateway", {}])
+    def get_path(self, target):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_path", {"target":target}])
+    def get_metrics(self, target):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_metrics", {"target":target}])
+    def get_gateway_metrics(self):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_gateway_metrics", {}])
+    def get_internet_path_metrics(self):
+        return self._send(method="call", params=[session_token,"/lime/api",
+                                                 "get_internet_path_metrics", {}])
     def close(self):
         self.connection.close()
 
@@ -74,7 +91,7 @@ if __name__ == "__main__":
     user = r"admin"
     password = r"admin"
 
-    ws = LiMeApi("ws://10.13.207.235/websocket/")
+    ws = LiMeApi("ws://10.5.0.6/websocket/")
 
     ws.list_methods()
     challenge_res = ws.challenge()
@@ -84,15 +101,27 @@ if __name__ == "__main__":
     ws.get_location()
     ws.get_neighbors()
     ws.get_bmx6()
-    ws.set_location(-31,-64)
-
+#    ws.set_location(-31,-64)
+    
     res = ws.get_interfaces()
     iface = json.loads(res)["result"]["interfaces"][0]
     ws.get_assoclist(iface)
-
-    res = ws.get_stations(iface)
-    station = json.loads(res)["result"].popitem()[0]
+    
+    res = ws.get_iface_stations(iface)
+    station = json.loads(res)["result"].popitem()[1]["mac"]
     res = ws.get_station_signal(iface, station)
- 
+    
+    res = ws.get_stations()
+    
+    res = ws.get_gateway()
+    gateway = json.loads(res)["result"]["gateway"]
+    path = json.loads(ws.get_path(gateway))
+    res = path["result"]
+    for i in range(1, len(res)+1):
+        ws.get_metrics(res[str(i)])
+
+    ws.get_gateway_metrics()
+    ws.get_internet_path_metrics()
+    
     ws.close()
 
